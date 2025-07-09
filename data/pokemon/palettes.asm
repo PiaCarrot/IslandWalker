b065d9ead5fa4f6ca7b68f78ca06582e34fdc0eb
vulcandth <vulcandth@gmail.com>
2025-07-08 19:05:46 -0500

    Fix some minor things post-merge

--- data/pokemon/palettes.asm
+++ data/pokemon/palettes.asm
@@ -646,10 +646,10 @@ INCBIN "gfx/pokemon/hoenn/latios/normal.gbcpal", middle_colors
 INCLUDE "gfx/pokemon/hoenn/latios/shiny.pal"
 INCBIN "gfx/pokemon/forms/lapras_partner/normal.gbcpal", middle_colors
 INCLUDE "gfx/pokemon/forms/lapras_partner/shiny.pal"
-INCBIN "gfx/pokemon/forms/dratini_partner/front.gbcpal", middle_colors
+INCBIN "gfx/pokemon/forms/dratini_partner/normal.gbcpal", middle_colors
 INCLUDE "gfx/pokemon/forms/dratini_partner/shiny.pal"
-INCBIN "gfx/pokemon/forms/eevee_partner/front.gbcpal", middle_colors
+INCBIN "gfx/pokemon/forms/eevee_partner/normal.gbcpal", middle_colors
 INCLUDE "gfx/pokemon/forms/eevee_partner/shiny.pal"
-INCBIN "gfx/pokemon/forms/pikachu_partner/front.gbcpal", middle_colors
+INCBIN "gfx/pokemon/forms/pikachu_partner/normal.gbcpal", middle_colors
 INCLUDE "gfx/pokemon/forms/pikachu_partner/shiny.pal"
 	assert_table_length NUM_POKEMON + 1
