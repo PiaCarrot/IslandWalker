5719d938cb99979ecc0fd8dfbb1b473297d89c78
PiaCarrot <datingsimsrs@gmail.com>
2025-09-04 17:58:45 -0400

    Fix Tauros form dimensions

--- data/pokemon/base_stats/forms/tauros_combat.asm
+++ data/pokemon/base_stats/forms/tauros_combat.asm
@@ -10,7 +10,7 @@
         dw NO_ITEM, NO_ITEM ; items
         db GENDER_F0 ; gender ratio
         db 20 ; step cycles to hatch
-        INCBIN "gfx/pokemon/kanto/tauros/front.dimensions"
+        INCBIN "gfx/pokemon/forms/tauros_combat/front.dimensions"
         db INTIMIDATE, ANGER_POINT ; wBaseAbility1, wBaseAbility2
         dw NULL ; Padding left-over from the unused back pic
         db GROWTH_SLOW ; growth rate
