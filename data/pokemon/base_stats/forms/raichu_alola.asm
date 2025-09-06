71f465cc5123fde7eebd8f40ace2f60e7d003656
PiaCarrot <datingsimsrs@gmail.com>
2025-09-06 01:40:46 -0400

    Add Lightningrod as 2nd ability for pikachu line

--- data/pokemon/base_stats/forms/raichu_alola.asm
+++ data/pokemon/base_stats/forms/raichu_alola.asm
@@ -11,7 +11,7 @@
 	db GENDER_F50 ; gender ratio
 	db 10 ; step cycles to hatch
 	INCBIN "gfx/pokemon/forms/raichu_alola/front.dimensions"
-	db SURGE_SURFER, LIGHTINGROD ; wBaseAbility1, wBaseAbility2
+	db SURGE_SURFER, LIGHTNINGROD ; wBaseAbility1, wBaseAbility2
 	dw NULL ; Padding left-over from the unused back pic
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_GROUND, EGG_FAIRY ; egg groups
