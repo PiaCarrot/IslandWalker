8995227b875968ce78caf0723e6ab093addb057a
PiaCarrot <datingsimsrs@gmail.com>
2025-09-13 01:45:02 -0400

    Challenge Mode sets trainer IVs to 31

--- engine/battle/read_trainer_ivs.asm
+++ engine/battle/read_trainer_ivs.asm
@@ -28,6 +28,22 @@ GetTrainerIVs:
 	ld [bc], a
 	pop bc
 
+	; If Challenge Mode is enabled, override IVs to 31 in every stat
+	ld a, [wOptions2]
+	bit CHALLENGE_MODE, a
+	jr z, .done
+
+	ld hl, wTempIVs
+	ld a, $7f ; Atk/Speed IVs = 31
+	ld [hli], a
+	ld a, $ff ; Def/Spd IVs = 31
+	ld [hli], a
+	ld a, $7f ; Sp.Atk/HP IVs = 31
+	ld [hli], a
+	ld a, $ff ; Sp.Def/HP IVs = 31
+	ld [hl], a
+
+.done
 	pop hl
 	ret
 
